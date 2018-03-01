b33f31945490f2a8c786667b3db5987aa9404adb
Christopher Kaag <christopher.kaag@gmail.com>
2018-03-01 22:20:53 +0100

    simple first commit

---
+++ src/main/kotlin/aemterliste/Main.kt
@@ -0,0 +1,13 @@
+package aemterliste
+
+import kotlin.browser.document
+
+
+val divAemterliste = document.getElementById("aemterliste")
+val divMailmanmails = document.getElementById("mailmanmailstxt")
+val divAemtermails = document.getElementById("aemtermailstxt")
+val divMails = document.getElementById("mailstxt")
+
+fun main(args : Array<String>) {
+    println("hello world")
+}
